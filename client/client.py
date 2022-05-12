d0774b65dad079dcf8bdd2ae5d824dab91c3cf4d
Simone Zama <simone.zama@studio.unibo.it>
2022-05-12 10:53:45 +0200

    Get input from user

--- client/client.py
+++ client/client.py
@@ -14,29 +14,37 @@ if __name__ == "__main__":
     message = 'first message'
     commands = []
 
+    # Get command list from server
+    try:
+        print (f'sending "{message}"')
+        sent = sock.sendto(message.encode(), server_address)
+
+        print('waiting for server response\n')
+        data, server = sock.recvfrom(4096)
+        received = data.decode('utf8')
+        print(received)
+
+        # Create available commands list
+        responseList = received.split('\n')[1:-1]
+        for word in responseList:
+            command = word.split()
+            commands.append(command[0])
+    except Exception as info:
+        print(info)
+        exit
+
     while True:
-        try:
-            print (f'sending "{message}"')
-            sent = sock.sendto(message.encode(), server_address)
+        inputCommand = input("Ender command: ")
+        sent = sock.sendto(inputCommand.encode(), server_address)
 
-            print('waiting for server response\n')
-            data, server = sock.recvfrom(4096)
-            received = data.decode('utf8')
-            print(received)
-
-            # Create available commands list
-            responseList = received.split('\n')[1:-1]
-            for word in responseList:
-                command = word.split()
-                commands.append(command[0])
-
-        except Exception as info:
-            print(info)
-
-        sent = sock.sendto("ls".encode(), server_address)
-        data = recFromServer(sock)
-        print(data)
-        break
+        if inputCommand == "ls":
+            data = recFromServer(sock)
+            print(data)
+        elif "get" in inputCommand:
+            # TODO
+            print("not implemented")
+        else:
+            break
 
     print ('closing socket')
     sock.close()
